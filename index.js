92fc719b8c5635a80cdebf0b0e8e717f1c27bd30
Josh Morris <jmo0624@gmail.com>
2020-09-01 22:37:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function createEmployeeRecord(arr) {
+  let obj = {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return obj
+}
+
+function createEmployees(arrayOfArrays) {
+  let theArray = []
+  arrayOfArrays.forEach(element => {
+    theArray.push(createEmployeeRecord(element))
+  });
+  return theArray
+}
+
+function createTimeInEvent(obj, timeStamp) {
+  let hour = parseInt(timeStamp.split(' ')[1])
+  let date = timeStamp.split(' ')[0]
+  obj.timeInEvents.push({type: "TimeIn", hour: hour, date: date})
+  return obj
+}
+
+function createTimeOutEvent(obj, timeStamp) {
+  let hour = parseInt(timeStamp.split(' ')[1])
+  let date = timeStamp.split(' ')[0]
+  obj.timeOutEvents.push(timeOut.hour - timeIn.hour) / 100
+  return result
+}
